de19d5ba9d7b7c7404bfb6b8e0db2f6dccb9adb9
Максим <makszavr.1234554321@yandex.ru>
2020-05-31 17:15:16 +0300

    Добавил переменную для хранения длины полосы

--- js/stat.js
+++ js/stat.js
@@ -7,7 +7,7 @@ var CLOUD_Y = 10;
 var GAP = 10;
 var FONT_GAP = 15;
 var TIME_GAP = 65;
-var BAR_HEIGHT = 150;
+var MAX_BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var COLUMT_GAP = 50;
 var winningPhrase = ['Ура вы победили!', 'Список результатов:'];
@@ -52,7 +52,8 @@ window.renderStatistics = function (ctx, players, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    var timeLeft = BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime;
+    var barHeight = MAX_BAR_HEIGHT * times[i] / maxTime;
+    var timeLeft = MAX_BAR_HEIGHT - (MAX_BAR_HEIGHT * times[i]) / maxTime;
     var leftColumnGap = CLOUD_X + BAR_WIDTH + (BAR_WIDTH + COLUMT_GAP) * i;
 
     if (players[i] === 'Вы') {
@@ -61,11 +62,11 @@ window.renderStatistics = function (ctx, players, times) {
       ctx.fillStyle = 'hsl(240, 100%, 30%)';
     }
 
-    ctx.fillRect(leftColumnGap, CLOUD_Y + TIME_GAP + timeLeft + FONT_GAP + GAP, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillRect(leftColumnGap, CLOUD_Y + TIME_GAP + timeLeft + FONT_GAP + GAP, BAR_WIDTH, barHeight);
 
     ctx.fillStyle = '#000';
 
-    ctx.fillText(players[i], leftColumnGap, CLOUD_Y + FONT_GAP * 2 + GAP + TIME_GAP + BAR_HEIGHT);
+    ctx.fillText(players[i], leftColumnGap, CLOUD_Y + FONT_GAP * 2 + GAP + TIME_GAP + MAX_BAR_HEIGHT);
     ctx.fillText(Math.round(times[i]), leftColumnGap, CLOUD_Y + timeLeft + FONT_GAP + TIME_GAP);
   }
 };
